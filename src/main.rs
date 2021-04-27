3effd8a08d808f67a588f1d184cb683850a4d987
williambl <william@williambl.com>
2021-04-27 15:43:48 +0100

    Add basic VM code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use crate::stack::Stack;
+use crate::instructions::Instruction;
 use std::io;
 use std::io::Read;
 use std::str::FromStr;
 
 mod stack;
+mod instructions;
 
 fn main() {
     let mut stack: Stack = Stack::new();
     let mut program: Vec<u32> = read_input();
 
-    while !program.is_empty() {
-        if let Some(x) = program.pop() {
-            println!("{}", x)
+    for packed_instr in program {
+        if let Some(mut instr) = Instruction::from_packed_instruction(packed_instr) {
+            instr.run(&mut stack)
         }
     }
+    println!("{}", stack.pop().unwrap_or_default())
 }
 
 fn read_input() -> Vec<u32> {
